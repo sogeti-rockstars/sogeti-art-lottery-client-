fe03a1145d78e2360bdad0ed79ab14bc88a379f8
lefterisYA <elefterios.lundell@yaelev.se>
2022-05-05 15:26:13 +0200

    Finished tests for art-item.service. Also made a nifty reporter in src/test.ts

--- src/app/service/art-item.service.spec.ts
+++ src/app/service/art-item.service.spec.ts
@@ -1,23 +1,29 @@
 import { HttpClientModule, HttpErrorResponse } from '@angular/common/http';
-import { fakeAsync, TestBed, tick } from '@angular/core/testing';
-import { lastValueFrom } from 'rxjs';
-import { Observable } from 'rxjs/internal/Observable';
+import { TestBed } from '@angular/core/testing';
+import { firstValueFrom, lastValueFrom } from 'rxjs';
 import { AppComponent } from '../app.component';
 import { ArtItem } from '../model/art-item';
-
 import { ArtItemService } from './art-item.service';
 
 describe('ArtItemService', () => {
   let service: ArtItemService;
-  let artItems: ArtItem[];
-  let component: AppComponent;
+  let listPreSpec: ArtItem[] | undefined;
+  let listPostSpec: ArtItem[] | undefined;
 
-  beforeEach(() => {
+  beforeEach(async () => {
     TestBed.configureTestingModule({
       declarations: [AppComponent],
       imports: [HttpClientModule],
     }).compileComponents();
     service = TestBed.inject(ArtItemService);
+    listPreSpec = undefined;
+    listPostSpec = undefined;
+    listPreSpec = await lastValueFrom(service.getArtItems());
+  });
+
+  afterEach(() => {
+    setSpecProperty('pre', listPreSpec);
+    setSpecProperty('post', listPostSpec);
   });
 
   it('should be created', () => {
@@ -25,101 +31,108 @@ describe('ArtItemService', () => {
   });
 
   it('should load an array of ArtItems', async () => {
-    var list = await lastValueFrom(service.getArtItems());
-    expect(list.length).toBeGreaterThanOrEqual(0);
-    expect(list).toBeDefined();
+    expect(listPreSpec).toBeDefined();
+    expect(listPreSpec!.length).toBeGreaterThanOrEqual(0);
   });
 
-  it('delete the last item of artItems', async () => {
-    var preList = await lastValueFrom(service.getArtItems());
-    if (preList.length == 0) return;
-    await lastValueFrom(service.deleteArtItem(preList[preList.length - 1].id));
-    var postList = await lastValueFrom(service.getArtItems());
-    expect(preList.length).toBe(postList.length + 1);
-    console.log('del last item done');
-    console.log({ pre: preList, post: postList });
+  it('should delete the last item of artItems', async () => {
+    expect(listPreSpec).toBeDefined();
+    if (listPreSpec!.length == 0) return;
+    await lastValueFrom(
+      service.deleteArtItem(listPreSpec![listPreSpec!.length - 1].id)
+    );
+    listPostSpec = await lastValueFrom(service.getArtItems());
+    expect(listPreSpec!.length).toBe(listPostSpec!.length + 1);
+    expect(listPreSpec![listPreSpec!.length - 2]).toEqual(
+      listPostSpec![listPostSpec!.length - 1]
+    ); // Next to last should be same as last
   });
 
-  it('delete all artItems', async () => {
-    await addNItems(10);
-    var currList = await lastValueFrom(service.getArtItems());
-    while (currList.length > 0) {
-      await lastValueFrom(
-        service.deleteArtItem(currList[currList.length - 1].id)
-      );
-      currList = await lastValueFrom(service.getArtItems());
+  it('should delete the item with a specified randomly selected existing id', async () => {
+    if (listPreSpec!.length == 0) listPreSpec = await addNItems(10);
+    let rItemFromGetAll =
+      listPreSpec![Math.floor(Math.random() * listPreSpec!.length)];
+    console.log(`Deleting item with id: ${rItemFromGetAll.id}`);
+    await lastValueFrom(service.deleteArtItem(rItemFromGetAll.id));
+    listPostSpec = await lastValueFrom(service.getArtItems());
+
+    var listDeletedReinserted = listPostSpec!
+      .concat([rItemFromGetAll])
+      .sort((a, b) => {
+        return a.id - b.id;
+      });
+    expect(listPreSpec!.length).toBe(listPostSpec!.length + 1);
+    expect(listDeletedReinserted).toEqual(
+      listPreSpec!.sort((a, b) => {
+        return a.id - b.id;
+      })
+    );
+  });
+
+  it('should delete all artItems', async () => {
+    // if ( listPreSpec!.length == 0 )
+    //   listPreSpec = await addNItems(10);
+    listPostSpec = listPreSpec!.slice();
+
+    while (listPostSpec!.length > 0) {
+      let lastItem = listPostSpec![listPostSpec!.length - 1];
+      await lastValueFrom(service.deleteArtItem(lastItem.id));
+      listPostSpec = await lastValueFrom(service.getArtItems());
     }
-    expect(currList.length).toBe(0); // Yah we would prob never get here if it ever were to fail...
+    expect(listPostSpec!.length).toBe(0); // Yah we would prob never get here if it ever were to fail...
   });
 
-  it('add an artItem', async () => {
-    var oldList = await lastValueFrom(service.getArtItems());
+  it('should add an artItem', async () => {
     await lastValueFrom(service.addArtItem(getRndmItem()));
-    var newList = await lastValueFrom(service.getArtItems());
-    expect(newList.length).toBe(oldList.length + 1);
-    console.log({ pre: oldList, post: newList });
+    listPostSpec = await lastValueFrom(service.getArtItems());
+    expect(listPostSpec.length).toBe(listPreSpec!.length + 1);
   });
 
-  it('add 10 artItems', async () => {
-    var itemsLists = await addNItems(10);
-    expect(itemsLists.post.length).toBe(itemsLists.pre.length + 10);
+  it('should add 10 artItems', async () => {
+    listPostSpec = await addNItems(10);
+    expect(listPostSpec.length).toBe(listPreSpec!.length + 10);
   });
 
-  type ItemList = {
-    pre: ArtItem[];
-    post: ArtItem[];
-  };
+  it('should get the item with a specified randomly selected existing id', async () => {
+    if (listPreSpec!.length == 0) listPreSpec = await addNItems(10);
+    let rItemFromGetAll =
+      listPreSpec![Math.floor(Math.random() * listPreSpec!.length)];
+    console.log(`Requesting item with id: ${rItemFromGetAll.id}`);
+    let rItemFromGetById = await lastValueFrom(
+      service.getArtItem(rItemFromGetAll.id)
+    );
 
-  async function addNItems(newItemsLen: number): Promise<ItemList> {
-    var oldList = await lastValueFrom(service.getArtItems());
+    expect(rItemFromGetAll).toEqual(rItemFromGetById);
+  });
+
+  it('should get an image from an item with a specified randomly selected existing id', async () => {
+    if (listPreSpec!.length == 0) listPreSpec = await addNItems(10);
+    let rItemFromGetAll =
+      listPreSpec![Math.floor(Math.random() * listPreSpec!.length)];
+    console.log(`Requesting image from item with id: ${rItemFromGetAll.id}`);
+
+    let image: Blob;
+    try {
+      image = await firstValueFrom(service.getArtItemImage(rItemFromGetAll.id));
+    } catch (e) {
+      if (e instanceof HttpErrorResponse) {
+        expect(e).toBeInstanceOf(HttpErrorResponse);
+        expect(e.status).toBe(404); // We choose id:s randomly, sometimes the items might not have a photo.
+      } else {
+        console.log(e);
+        fail();
+      }
+      return;
+    }
+
+    expect(image!.type).toBe('image/jpeg');
+  });
+
+  async function addNItems(newItemsLen: number): Promise<ArtItem[]> {
     for (let i = 0; i < newItemsLen; i++) {
       await lastValueFrom(service.addArtItem(getRndmItem()));
     }
-    var newList = await lastValueFrom(service.getArtItems());
-    console.log(`old: ${oldList.length}, new: ${newList.length}`);
-    return { pre: oldList, post: newList };
-  }
-
-  //   loadItems(() => {
-  //     deleteAllItems(() => {
-  //       loadItems(() => {
-  //         expect(artItems.length).toBe(0);
-  //
-  //         for (let i = 0; i < 10; i++) {
-  //           loadItems(() => {
-  //             var oldLength = artItems.length;
-  //             var newLength = oldLength + 1;
-  //
-  //             addAnItem(() => {
-  //               loadItems(() => {
-  //                 expect(artItems.length).toBe(newLength);
-  //               });
-  //             });
-  //           });
-  //         }
-  //
-  //         loadItems(() => {
-  //           expect(artItems.length).toBe(10);
-  //         });
-  //         done();
-  //       });
-  //     });
-  //   });
-
-  function loadItems(onLoadingFinished: Function): void {
-    service.getArtItems().subscribe({
-      complete: () => {
-        console.log('loadItems complete:');
-        onLoadingFinished();
-      },
-      error: (error: HttpErrorResponse) => {
-        console.log('loadItems HttpErrorResponse:');
-        console.log(error.message);
-      },
-      next: (resp: ArtItem[]) => {
-        artItems = resp;
-      },
-    });
+    return await lastValueFrom(service.getArtItems());
   }
 
   function getRndmItem(): ArtItem {
@@ -132,84 +145,5 @@ describe('ArtItemService', () => {
     randItem.technique = 'addAnItemRandTechnique';
     randItem.value = 'addAnItem999kr';
     return randItem;
-  }
-
-  function addAnItem(onAddFinished: Function): void {
-    service.addArtItem(getRndmItem()).subscribe({
-      complete: () => {
-        console.log('deleteItem complete:');
-        onAddFinished();
-      },
-      error: (error: HttpErrorResponse) => {
-        console.log('deleteItem HttpErrorResponse:');
-        console.log(error.message);
-      },
-      next: (resp: any) => {
-        console.log('deleteItem Resp:');
-        console.log(resp);
-      },
-    });
-  }
-
-  function deleteLastItem(onComplete: Function): void {
-    if (artItems.length == 0) {
-      loadItems(onComplete);
-      return;
-    }
-
-    service.deleteArtItem(artItems[artItems.length - 1].id).subscribe({
-      complete: () => {
-        console.log('deleteItem complete:');
-        onComplete();
-      },
-      error: (error: HttpErrorResponse) => {
-        console.log('deleteItem HttpErrorResponse:');
-        console.log(error.message);
-      },
-      next: (resp: any) => {
-        console.log('deleteItem Resp:');
-        console.log(resp);
-      },
-    });
-  }
-
-  function deleteAllItems(callback: Function): void {
-    loadItems(() => {
-      deleteLastItem(() => {
-        if (artItems.length == 0) {
-          callback();
-        } else {
-          deleteAllItems(callback);
-        }
-      });
-    });
-  }
-
-  var obsCompleted: boolean = true;
-  function awaitObservable(obs: Observable<any>): void {
-    obsCompleted = false;
-    obs.subscribe({
-      complete: () => {
-        console.log('awaitObservable complete.');
-        obsCompleted = true;
-      },
-      error: (error: any) => {
-        console.log('awaitObservable error:');
-        console.log(error);
-      },
-      next: (resp: any) => {
-        console.log('deleteItem Resp:');
-        console.log(resp);
-      },
-    });
-
-    // while (!obsCompleted) {
-    //   delay(10);
-    //   setTimeout(10, 10);
-    // }
-  }
-
-  function delay(ms: number) {
-    return new Promise((resolve) => setTimeout(resolve, ms));
   }
 });
