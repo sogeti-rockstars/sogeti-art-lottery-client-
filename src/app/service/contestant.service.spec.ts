533824c6599dd15eabbf1e8cacbaaea8a2ae91e0
lefterisYA <elefterios.lundell@yaelev.se>
2022-05-05 15:41:49 +0200

    Completet API. Code is untested

--- src/app/service/contestant.service.spec.ts
+++ src/app/service/contestant.service.spec.ts
@@ -1,12 +1,15 @@
+import { HttpClientModule } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
-
 import { ContestantService } from './contestant.service';
 
 describe('ContestantService', () => {
   let service: ContestantService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      declarations: [],
+      imports: [HttpClientModule],
+    }).compileComponents();
     service = TestBed.inject(ContestantService);
   });
 
