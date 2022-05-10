22692690c8490ce1c7f0a3ec910ffb1a97a4fb36
ottomann1 <otto.kostmann@hotmail.com>
2022-05-10 09:24:39 +0200

    bootstrap borta'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,6 @@ import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { ModalInfoComponent } from './component/modal/modal-info/modal-info.component';
 import { ModalService } from './component/modal/modal.service';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { ReactiveFormsModule } from '@angular/forms';
 import { ArtItemFormComponent } from './component/form/art-item-form/art-item-form.component';
 import { ArtItemService } from './service/art-item.service';
@@ -36,8 +35,7 @@ import {MatCardModule} from '@angular/material/card';
   imports: [
     BrowserModule, 
     AppRoutingModule, 
-    HttpClientModule, 
-    NgbModule,
+    HttpClientModule,
     ReactiveFormsModule,
     BrowserAnimationsModule,
     MatDialogModule,
@@ -47,6 +45,6 @@ import {MatCardModule} from '@angular/material/card';
     MatCardModule
   ],
   providers: [ModalService, ArtItemService],
-  bootstrap: [AppComponent],
+  bootstrap: [AppComponent]
 })
 export class AppModule {}
