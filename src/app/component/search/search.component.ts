4342fcb3fb8d7fc5b6e1d17c369625f7a8a65530
jessica pamlin <jessicapamlins@hotmail.se>
2022-05-12 13:48:06 +0200

    fixat bättre search komponent

--- src/app/component/search/search.component.ts
+++ src/app/component/search/search.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-search',
@@ -6,7 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search.component.css'],
 })
 export class SearchComponent implements OnInit {
+  public searchQuery: string = '';
+  public searchForm!: FormGroup;
+
+  @Output() onSearch: EventEmitter<string> = new EventEmitter<string>();
+  
   constructor() {}
 
   ngOnInit(): void {}
+
+  public search() {
+    console.log(this.searchForm.get('query')!.value);
+  }
 }
+
+function keyDownFunction(event: Event | undefined, any: any) {
+    throw new Error('Function not implemented.');
+  }
