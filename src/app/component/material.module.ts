ef48baa2183a1016a8d76c740e107148274f285e
ottomann1 <otto.kostmann@hotmail.com>
2022-05-13 12:21:31 +0200

    Revert "Mostly comments and removal of bad code"
    This reverts commit f6166224fa3536aec8ec256466aee42342bba760.

--- src/app/component/material.module.ts
+++ src/app/component/material.module.ts
@@ -29,7 +29,7 @@ import { MaterialArtItemFormComponent } from './form/material-art-item-form/mate
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatInputModule } from '@angular/material/input';
 import { MatCardModule } from '@angular/material/card';
-import { MatCommonModule, MatLineModule, MatOptionModule, MatPseudoCheckboxModule } from '@angular/material/core';
+import { MatCommonModule, MatLineModule, MatOptionModule } from '@angular/material/core';
 import { ContestantRowComponent } from './contestant-row/contestant-row.component';
 import { SpinnerComponent } from '../layouts/spinner/spinner.component';
 import { ContestantsComponent } from '../pages/contestants/contestants.component';
@@ -44,6 +44,7 @@ import { MatTableModule } from '@angular/material/table';
 // import { MatAutocompleteModule } from '@angular/material/autocomplete';
 // import { MatButtonToggleModule } from '@angular/material/button-toggle';
 // import { MatCardModule } from '@angular/material/card';
+// import { MatCheckboxModule } from '@angular/material/checkbox';
 // import { MatChipsModule } from '@angular/material/chips';
 // import { MatDatepickerModule } from '@angular/material/datepicker';
 // import { MatDialogModule } from '@angular/material/dialog';
@@ -58,6 +59,7 @@ import { MatTableModule } from '@angular/material/table';
 // import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 // import { MatSnackBarModule } from '@angular/material/snack-bar';
 // import { MatSortModule } from '@angular/material/sort';
+// import { MatTableModule } from '@angular/material/table';
 // import { MatTabsModule } from '@angular/material/tabs';
 // import { MatTooltipModule } from '@angular/material/tooltip';
 // import { MatStepperModule } from '@angular/material/stepper';
@@ -87,9 +89,6 @@ const modules: Array<any> = [
     MatCommonModule,
     MatLineModule,
     MatMenuModule,
-    MatCheckboxModule,
-    MatPseudoCheckboxModule,
-    MatTableModule,
 ];
 
 const components: Array<any> = [
