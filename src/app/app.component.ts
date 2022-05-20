74c9c9c8240a10377c40973f589ce1aa6ebf37d5
ottomann1 <92720809+ottomann1@users.noreply.github.com>
2022-05-20 09:35:24 +0200

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -44,61 +44,10 @@ export class AppComponent implements OnInit {
 
   imageToShow: any;
 
-  // <<<<<<< material_bootstrap_mush
-  //   public getImageUrl(id: number): string {
-  //     return this.artItemService.getArtItemImageUrl(id);
-  // =======
   public getImageUrl(id: number): string {
     return this.artItemApiService.getArtItemImageUrl(id);
   }
 
-  //WIP: loading an image from blob
-  // createImageFromBlob(image: Blob) {
-  //    let reader = new FileReader();
-  //    reader.addEventListener("load", () => {
-  //       this.imageToShow = reader.result;
-  //    }, false);
-
-  //    if (image) {
-  //       reader.readAsDataURL(image);
-  //    }
-  // }
-
-  //   getImageFromService(artItem:ArtItem) {
-  //     this.isImageLoading = true;
-  //     this.artItemApiService.getArtItemImage(artItem.id).subscribe(data => {
-  //       this.createImageFromBlob(data);
-  //       this.isImageLoading = false;
-  //     }, error => {
-  //       this.isImageLoading = false;
-  //       console.log(error);
-  //     });
-  // }
-
-  // public addArtItem(e: any){
-  //   e.preventDefault();
-  //   const artItem = new ArtItem;
-  //   this.modalService.setRootViewContainerRef(this.viewContainerRef);
-  //   this.modalService.itemModal(artItem, `Add new item`);
-  // }
-
-  // public updateArtItem(e: any, artItem: ArtItem){
-  //   e.preventDefault();
-  //   this.modalService.setRootViewContainerRef(this.viewContainerRef);
-  //   this.modalService.itemModal(artItem, `Update "${artItem.itemName}"`);
-  // }
-
-  // public editImageModalView(e:any, artItem:ArtItem){
-  //   e.preventDefault();
-  //   this.modalService.setRootViewContainerRef(this.viewContainerRef);
-  //   this.modalService.editItemImageModal(artItem, 'Image modal');
-  // }
-
-  // public imageModalView(e:any, artItem:ArtItem){
-  //   e.preventDefault();
-  //   this.modalService.setRootViewContainerRef(this.viewContainerRef);
-  //   this.modalService.itemImageModal(artItem, 'Image modal');
-  // }
 
   public deleteArtItem(e: any, artItem: ArtItem) {
     e.preventDefault();
