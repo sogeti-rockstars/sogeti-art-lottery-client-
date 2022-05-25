6f3945f3612de7a5fbe60c487f71d8c44aa7f9ff
lefterisYA <elefterios.lundell@yaelev.se>
2022-05-25 12:42:15 +0200

    done!

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,51 +10,46 @@ import { ArtItemService } from './service/art-item.service';
     styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
-  title = 'sogeti-art-lottery-client';
-  paintings: ArtItem[] = [];
-  isImageLoading: boolean = false;
-  // isAdmin: boolean = location.search.indexOf('adminview') != -1;
+    title = 'sogeti-art-lottery-client';
+    paintings: ArtItem[] = [];
+    isImageLoading: boolean = false;
+    // isAdmin: boolean = location.search.indexOf('adminview') != -1;
 
-  constructor(
-    private artItemApiService: ArtItemApiService,
-    private artItemService: ArtItemService,
-    private viewContainerRef: ViewContainerRef
-  ) {}
-  ngOnInit(): void {
-    this.loadPaintings();
-    this.artItemService.artItemSubject$.subscribe(() => {
-      this.loadPaintings();
-    });
-  }
-
-  // Example code on how to use painting service:
-  public loadPaintings(): void {
-    this.artItemApiService.getArtItems().subscribe({
-      complete: () => {
-        console.log('Loading complete!');
-      },
-      error: (error: HttpErrorResponse) => {
-        alert(error.message);
-      },
-      next: (resp: ArtItem[]) => {
-        this.paintings = resp;
-      },
-    });
-  }
-
-  imageToShow: any;
-
-  public getImageUrl(id: number): string {
-    return this.artItemApiService.getArtItemImageUrl(id);
-  }
-
-
-  public deleteArtItem(e: any, artItem: ArtItem) {
-    e.preventDefault();
-    this.artItemApiService.deleteArtItem(artItem.id).subscribe({
-      complete: () => {
+    constructor(private artItemApiService: ArtItemApiService, private artItemService: ArtItemService, private viewContainerRef: ViewContainerRef) {}
+    ngOnInit(): void {
         this.loadPaintings();
-      },
-    });
-  }
+        this.artItemService.artItemSubject$.subscribe(() => {
+            this.loadPaintings();
+        });
+    }
+
+    // Example code on how to use painting service:
+    public loadPaintings(): void {
+        this.artItemApiService.getArtItems().subscribe({
+            complete: () => {
+                console.log('Loading complete!');
+            },
+            error: (error: HttpErrorResponse) => {
+                alert(error.message);
+            },
+            next: (resp: ArtItem[]) => {
+                this.paintings = resp;
+            },
+        });
+    }
+
+    imageToShow: any;
+
+    public getImageUrl(id: number): string {
+        return this.artItemApiService.getArtItemImageUrl(id);
+    }
+
+    public deleteArtItem(e: any, artItem: ArtItem) {
+        e.preventDefault();
+        this.artItemApiService.deleteArtItem(artItem.id).subscribe({
+            complete: () => {
+                this.loadPaintings();
+            },
+        });
+    }
 }
--- src/app/service/art-item.service.ts
+++ src/app/service/art-item.service.ts
@@ -6,25 +6,33 @@ import { ArtItem } from '../model/art-item';
 import { ArtItemApiService } from './api/art-item-api.service';
 
 @Injectable({
-  providedIn: 'root',
+    providedIn: 'root',
 })
 export class ArtItemService {
-  private artItemSubject: Subject<ArtItem> = new Subject();
-  public artItemSubject$ = this.artItemSubject.asObservable();
-  paintings: ArtItem[] = [];
+    private artItemSubject: Subject<ArtItem> = new Subject();
+    public artItemSubject$ = this.artItemSubject.asObservable();
+    paintings: ArtItem[] = [];
 
-  constructor(private artItemApiService: ArtItemApiService) {}
+    constructor(private artItemApiService: ArtItemApiService) {}
 
-  public observeUpdateArtItem(artItem: ArtItem): Observable<ArtItem> {
-    return this.artItemApiService.updateArtItem(artItem).pipe(
-      retry(1),
-      tap((response) => this.artItemSubject.next(artItem))
-    );
-  }
-  public observeAddArtItem(artItem: ArtItem): Observable<ArtItem> {
-    return this.artItemApiService.addArtItem(artItem).pipe(
-      retry(1),
-      tap((response) => this.artItemSubject.next(artItem))
-    );
-  }
+    public observeUpdateArtItem(artItem: ArtItem): Observable<ArtItem> {
+        return this.artItemApiService.updateArtItem(artItem).pipe(
+            retry(1),
+            tap((response) => this.artItemSubject.next(artItem))
+        );
+    }
+    public observeAddArtItem(artItem: ArtItem): Observable<ArtItem> {
+        return this.artItemApiService.addArtItem(artItem).pipe(
+            retry(1),
+            tap((response) => this.artItemSubject.next(artItem))
+        );
+    }
+
+    public getArtItems(): Observable<ArtItem[]> {
+        return this.artItemApiService.getArtItems();
+    }
+
+    public deleteArtItem(id: number): Observable<Object> {
+        return this.artItemApiService.deleteArtItem(id);
+    }
 }
