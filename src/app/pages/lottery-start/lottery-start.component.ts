e94edbebd6697f733836a1773e3c4ee16e6bfef5
lefterisYA <elefterios.lundell@yaelev.se>
2022-05-30 14:31:33 +0200

    Modified contestants.component so that contestants[] are now loaded by parent component. Also renamed it to ContestantListComponent

--- src/app/pages/lottery-start/lottery-start.component.ts
+++ src/app/pages/lottery-start/lottery-start.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Contestant } from 'src/app/model/contestant';
+import { Lottery } from 'src/app/model/lottery';
 import { Winner } from 'src/app/model/winner';
+import { ContestantService } from 'src/app/service/contestant.service';
 import { LotteryService } from 'src/app/service/lottery.service';
 
 @Component({
@@ -8,15 +11,25 @@ import { LotteryService } from 'src/app/service/lottery.service';
     styleUrls: ['./lottery-start.component.css'],
 })
 export class LotteryStartComponent implements OnInit {
-    winners: Winner[] = [];
-    constructor(private lotteryService: LotteryService) {}
+    winners: Contestant[] = [];
+    contestants: Contestant[] = [];
 
-    ngOnInit(): void {}
+    constructor(private lotteryService: LotteryService, private contestantService: ContestantService) {}
+
+    ngOnInit(): void {
+        this.lotteryService.lotteryChanged.subscribe((lott) => {
+            this.contestantService.getContestants(lott.id).subscribe((conts) => (this.contestants = conts));
+        });
+    }
 
     spinTheWheel() {
-        this.lotteryService.spinTheWheel(1).subscribe((data: Winner) => {
-            this.winners.push(data);
-            console.log(data.id);
-        });
+        this.winners.push(this.getRandomContestant());
+    }
+
+    private getRandomContestant() {
+        let max = this.contestants.length - 1;
+        let randomId = Math.floor(Math.random() * max);
+        let randomCont = this.contestants.splice(randomId, 1);
+        return randomCont[0];
     }
 }
