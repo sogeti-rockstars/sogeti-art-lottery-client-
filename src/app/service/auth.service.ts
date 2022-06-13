813312f74908b69ef266157520948f4533864f8a
lefterisYA <elefterios.lundell@yaelev.se>
2022-06-13 12:46:32 +0200

    Looks like more changes than it is: we only needed to set "withCredentials: true" in httpclient when we verify that we are logged in....

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -7,38 +7,54 @@ import { environment } from 'src/environments/environment';
     providedIn: 'root',
 })
 export class AuthService {
+    public get authenticated() {
+        return this.authenticated$;
+    }
+    public readonly loginLogoutChanged = new EventEmitter<boolean>();
+
+    private authenticated$ = false;
     private apiServerUrl = environment.apiBaseUrl;
-    authenticated = false;
-    private authHeaders$!: HttpHeaders | undefined;
-    get authHeaders() {
-        return this.authHeaders$;
+    private authHeaders!: HttpHeaders;
+
+    constructor(private http: HttpClient, private router: Router) {
+        this.checkAuthentication(); // this.login('admin', 'admin');
     }
 
-    public loginLogoutChanged = new EventEmitter<boolean>();
-
-    constructor(private http: HttpClient, private router: Router) {}
-
     login(user: string, pass: string) {
-        this.authHeaders$ = new HttpHeaders();
-        this.authHeaders$ = this.authHeaders$.set('Authorization', 'Basic ' + btoa(`${user}:${pass}`));
+        this.authHeaders = new HttpHeaders();
+        this.authHeaders = this.authHeaders.set('Authorization', 'Basic ' + btoa(`${user}:${pass}`));
 
-        this.http.get(`${this.apiServerUrl}/api/v1/users/current/`, { headers: this.authHeaders }).subscribe({
+        this.http.get(`${this.apiServerUrl}/api/v1/users/current`, { headers: this.authHeaders, observe: 'response' }).subscribe({
             next: (_) => {
-                this.authenticated = true;
+                this.authenticated$ = true;
                 this.loginLogoutChanged.emit(true);
             },
             error: (_) => {
-                this.authHeaders$ = undefined;
+                this.authHeaders = new HttpHeaders();
             },
+            complete: () => {},
         });
     }
 
     logout() {
         this.router.navigateByUrl('/');
-        this.http.put(`${this.apiServerUrl}/api/v1/users/logout`, '', { headers: this.authHeaders }).subscribe((r) => {
-            this.authenticated = false;
-            this.authHeaders$ = undefined;
+        this.http.put(`${this.apiServerUrl}/api/v1/users/logout`, '', this.getHttpOptions()).subscribe((_) => {
+            this.authenticated$ = false;
+            this.authHeaders = new HttpHeaders();
             this.loginLogoutChanged.emit(false);
+        });
+    }
+
+    getHttpOptions(): { headers?: HttpHeaders; withCredentials?: boolean | undefined } {
+        return { headers: this.authHeaders, withCredentials: this.authenticated };
+    }
+
+    private checkAuthentication() {
+        this.http.get<any>(`${this.apiServerUrl}/api/v1/users/current`, { headers: this.authHeaders, withCredentials: true }).subscribe({
+            next: (r) => {
+                console.log(r.name);
+                if (r.name === 'admin') this.authenticated$ = true;
+            },
         });
     }
 }
